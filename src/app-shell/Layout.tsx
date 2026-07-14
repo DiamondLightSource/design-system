024f840d02e517dbdbed9fb8df602ab4b9759ba6
Zohar Manor-Abel <zohar.manor-abel@diamond.ac.uk>
2026-07-14 16:00:25 +0100

    Added and udpated
    - Added ability to show a page with no tabs
    - Added an app page title
    - Added home page

--- src/app-shell/Layout.tsx
+++ src/app-shell/Layout.tsx
@@ -1,10 +1,28 @@
 import { Toolbar } from "@mui/material";
 import Box from "@mui/material/Box";
-import { NavLink, Outlet } from "react-router-dom";
-import { routePath, type RouterProps } from "./Router";
+import { useEffect } from "react";
+import { NavLink, Outlet, useMatches } from "react-router-dom";
+import { routePath, type RouteHandle, type RouterProps } from "./Router";
 import { SidebarNav, type Navigation } from "./SidebarNav";
 import { TopBar } from "./TopBar";
 import { usePersistentDrawerState } from "./usePersistentDrawerState";
+
+const pageTitleSeparator = " · ";
+
+/** Sets `document.title` to "<app title> <separator> <page title>" */
+function usePageTitle(appTitle: string) {
+  const matches = useMatches();
+  const pageTitle = [...matches]
+    .reverse()
+    .map((match) => (match.handle as RouteHandle | undefined)?.title)
+    .find((title) => title !== undefined);
+
+  useEffect(() => {
+    document.title = pageTitle
+      ? `${pageTitle}${pageTitleSeparator}${appTitle}`
+      : appTitle;
+  }, [appTitle, pageTitle]);
+}
 
 export function toNavItemGroups(routerProps: RouterProps): Navigation {
   return routerProps.navigation.map((group) => ({
@@ -24,6 +42,7 @@ export function Layout(props: RouterProps) {
   const { open, setOpen } = usePersistentDrawerState();
 
   const navigation = toNavItemGroups(props);
+  usePageTitle(props.title);
 
   return (
     <Box sx={{ display: "flex" }}>
