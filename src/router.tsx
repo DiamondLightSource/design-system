455b4855203ca838d90eaa4b165271c607e33890
Zohar Manor-Abel <zohar.manor-abel@diamond.ac.uk>
2026-07-14 17:56:35 +0100

    Updated Home and structure
    - Home is now ready as a landing page
    - Added page structure for other areas

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,34 @@
-import { LucideHome, LucidePalette, LucideWorm } from "lucide-react";
+import {
+  Home as HomeIcon,
+  Palette,
+  Shapes,
+  Blocks,
+  ShieldCheck,
+  Rocket,
+} from "lucide-react";
 import { createRouter, type SectionGroup } from "./app-shell";
+
 import Home from "./pages/Home.mdx";
-import Design from "./pages/Introduction/Design.mdx";
-import More from "./pages/Introduction/More.mdx";
+
+import Design from "./pages/GettingStarted/Design.mdx";
+import More from "./pages/GettingStarted/More.mdx";
+
+import Foundations from "./pages/Foundations/Overview.mdx";
 import Colours from "./pages/Foundations/Colours.mdx";
 import Typography from "./pages/Foundations/Typography.mdx";
+
+import Components from "./pages/Components/Overview.mdx";
+import Inputs from "./pages/Components/Inputs.mdx";
+import Navigation from "./pages/Components/Navigation.mdx";
+
+import Patterns from "./pages/Patterns/Overview.mdx";
+import ApplicationStructure from "./pages/Patterns/ApplicationStructure.mdx";
+import NavigationP from "./pages/Patterns/Navigation.mdx";
+
+import Standards from "./pages/Standards/Overview.mdx";
+import Accessibility from "./pages/Standards/Accessibility.mdx";
+import InteractionStandards from "./pages/Standards/InteractionStandards.mdx";
+
 import MdxWrapper from "./Wrapper";
 
 const navigation: SectionGroup[] = [
@@ -12,14 +36,14 @@ const navigation: SectionGroup[] = [
     sections: [
       {
         name: "Home",
-        icon: <LucideHome />,
+        icon: <HomeIcon />,
         path: "Home",
         element: <MdxWrapper component={Home} />,
       },
       {
-        name: "Introduction",
-        icon: <LucideWorm />,
-        path: "introduction",
+        name: "Getting Started",
+        icon: <Rocket />,
+        path: "GettingStarted",
         pages: [
           {
             name: "Design",
@@ -33,8 +57,12 @@ const navigation: SectionGroup[] = [
       },
       {
         name: "Foundations",
-        icon: <LucidePalette />,
+        icon: <Palette />,
         pages: [
+          {
+            name: "Overview",
+            element: <MdxWrapper component={Foundations} />,
+          },
           {
             name: "Colours",
             element: <MdxWrapper component={Colours} />,
@@ -42,6 +70,60 @@ const navigation: SectionGroup[] = [
           {
             name: "Typography",
             element: <MdxWrapper component={Typography} />,
+          },
+        ],
+      },
+      {
+        name: "Components",
+        icon: <Shapes />,
+        pages: [
+          {
+            name: "Overview",
+            element: <MdxWrapper component={Components} />,
+          },
+          {
+            name: "Inputs",
+            element: <MdxWrapper component={Inputs} />,
+          },
+          {
+            name: "Navigation",
+            element: <MdxWrapper component={Navigation} />,
+          },
+        ],
+      },
+      {
+        name: "Patterns",
+        icon: <Blocks />,
+        pages: [
+          {
+            name: "Overview",
+            element: <MdxWrapper component={Patterns} />,
+          },
+          {
+            name: "Application Structure",
+            element: <MdxWrapper component={ApplicationStructure} />,
+          },
+          {
+            name: "Navigation",
+            element: <MdxWrapper component={NavigationP} />,
+          },
+        ],
+      },
+      {
+        name: "Standards",
+        icon: <ShieldCheck />,
+        pages: [
+          {
+            name: "Overview",
+            element: <MdxWrapper component={Standards} />,
+          },
+          {
+            name: "Accessibility",
+            element: <MdxWrapper component={Accessibility} />,
+          },
+          {
+            name: "Interaction Standards",
+            element: <MdxWrapper component={InteractionStandards} />,
           },
         ],
       },
